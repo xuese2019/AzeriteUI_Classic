00548ba59fd47b42af3c642890c6532b1616efe6
Lars Norberg <gitkraken@larsnorbergofficial.com>
2019-09-18 07:08:00 +0200

    move worldmap mods to own file and module

---
+++ AzeriteUI_Classic/front-end/blizzard/worldmap.lua
@@ -0,0 +1,13 @@
+local ADDON = ...
+local Core = CogWheel("LibModule"):GetModule(ADDON)
+if (not Core) then 
+	return 
+end
+
+local Module = Core:NewModule("BlizzardWorldMap", "LibEvent", "LibBlizzard")
+Module:SetIncompatible("ClassicWorldMapEnhanced")
+Module:SetIncompatible("Leatrix_Maps")
+
+Module.OnEnable = function(self)
+	self:DisableUIWidget("WorldMap")
+end 
