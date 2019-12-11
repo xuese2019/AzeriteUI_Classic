85bd4d73501921fa87924c5943962ed7d26f3c25
Lars Norberg <gitkraken@larsnorbergofficial.com>
2019-12-11 10:32:50 +0100

    Only apply our spam removal to system messages.

--- AzeriteUI_Classic/front-end/custom/chatfilters.lua
+++ AzeriteUI_Classic/front-end/custom/chatfilters.lua
@@ -43,14 +43,8 @@ end
 
 Module.OnEnable = function(self)
 	ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", OnChatMessage)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID_BOSS_EMOTE", OnChatMessage)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", OnChatMessage)
-	ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", OnChatMessage)
 end
 
 Module.OnDisable = function(self)
 	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_SYSTEM", OnChatMessage)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_RAID_BOSS_EMOTE", OnChatMessage)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_CHANNEL", OnChatMessage)
-	ChatFrame_RemoveMessageEventFilter("CHAT_MSG_YELL", OnChatMessage)
 end
