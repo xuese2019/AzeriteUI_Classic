08fc056bdb04828097de5903400e844e113810e2
Lars Norberg <gitkraken@larsnorbergofficial.com>
2020-02-06 10:30:04 +0100

    setting a cvar that for some reason has no effect. wth?

--- AzeriteUI_Classic/front-end/blizzard/worldmap.lua
+++ AzeriteUI_Classic/front-end/blizzard/worldmap.lua
@@ -8,6 +8,15 @@ local Module = Core:NewModule("BlizzardWorldMap", "LibEvent", "LibBlizzard")
 Module:SetIncompatible("ClassicWorldMapEnhanced")
 Module:SetIncompatible("Leatrix_Maps")
 
+Module.OnInit = function(self)
+	-- Report system is tainted when we move the Map,
+	-- so we're attempting to just remove the whole thing instad.
+	SetCVar("enablePVPNotifyAFK","0")
+end
+
 Module.OnEnable = function(self)
+	-- This does NOT disable the map,
+	-- but rather shrink it and adds some
+	-- conveniences like coordinates and movement fading.
 	self:DisableUIWidget("WorldMap")
 end 
