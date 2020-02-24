f4698dcf275d98004e15549d88291c1755e4cede
Lars Norberg <gitkraken@larsnorbergofficial.com>
2020-02-24 17:04:35 +0100

    color debuffs on hostile units red

--- AzeriteUI_Classic/front-end/stylesheet.lua
+++ AzeriteUI_Classic/front-end/stylesheet.lua
@@ -1405,7 +1405,7 @@ end
 local UnitFrame_Aura_PostUpdateButton = function(element, button)
 	local colors = element._owner.colors
 	local layout = element._owner.layout
-	if UnitIsFriend("player", button.unit) then 
+	if UnitIsFriend("player", button.unit) then
 		if button.isBuff then 
 			local color = layout.AuraBorderBackdropBorderColor
 			if color then 
@@ -1429,7 +1429,8 @@ local UnitFrame_Aura_PostUpdateButton = function(element, button)
 				button.Border:SetBackdropBorderColor(color[1], color[2], color[3])
 			end 
 		else
-			local color = colors.debuff[button.debuffType or "none"] or layout.AuraBorderBackdropBorderColor
+			--local color = colors.debuff[button.debuffType or "none"] or layout.AuraBorderBackdropBorderColor
+			local color = colors.debuff.none or layout.AuraBorderBackdropBorderColor
 			if color then 
 				button.Border:SetBackdropBorderColor(color[1], color[2], color[3])
 			end 
